02c1e7b7cb943da18fc207cc28cf4fa3850190d1
Jack黃老師 <yjhwang59@gmail.com>
2022-08-03 08:51:35 +0800

    Add files via upload

---
+++ departAdd.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="departAdd.aspx.cs" Inherits="WebForm1.departAdd" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+<form id="form1" runat="server">
+<div id="AlertMessage" title="信息确认">
+    <p id="AlertMessageBody"  class="msgbody"></p>
+</div>
+<div id="ConfirmMessage" title="信息提问">
+    <p id="ConfirmMessageBody" class="msgbody""></p>
+</div>
+        <div>
+            <asp:Label ID="lbTitle" runat="server" Font-Size="X-Large" Text="新增部門資料"></asp:Label>
+            <br />
+            <br />
+            <asp:Label ID="Label1" runat="server" Text="編號"></asp:Label>
+            <asp:TextBox ID="txtID" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="名稱"></asp:Label>
+            <asp:TextBox ID="txtName" runat="server" Width="260px"></asp:TextBox>
+            <br />
+            <asp:Label ID="Label3" runat="server" Text="備註"></asp:Label>
+            <asp:TextBox ID="txtRemark" runat="server" Height="63px" Width="262px"></asp:TextBox>
+            <br />
+            <asp:Button ID="btnCancel" runat="server" OnClick="btnCancel_Click" Text="取消" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="btnOK" runat="server" OnClick="btnOK_Click" Text="確定" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ departAdd.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebForm1
+{
+    public partial class departAdd : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        string sqlString;
+        protected void btnOK_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection cn = new SqlConnection())
+                {
+                    cn.ConnectionString = @"Server=localhost\SQLEXPRESS;" +
+                        "database=DB1; Integrated Security=True";
+                    cn.Open();
+                    sqlString = "INSERT INTO depart (depNo, depName, depRemark) ";
+                    sqlString += " Values(@dNo, @dName, @dRemark)";
+                    SqlCommand cmd = new SqlCommand(sqlString, cn);
+
+                    cmd.Parameters.AddWithValue("@dNo", txtID.Text);
+                    cmd.Parameters.AddWithValue("@dName", txtName.Text);
+                    cmd.Parameters.AddWithValue("@dRemark", txtRemark.Text);
+                    cmd.ExecuteNonQuery();
+                    cn.Close();
+                }
+                // MessageBox.Show("新增資料, 使用SQLparameter, OK!");
+
+                // System.Web.UI.ScriptManager.RegisterStartupScript(this, this.GetType(), "OpenNewWindow", "alert('新增資料OK！');", true);
+                ShowMsg('完成新增!', function(yes) {
+
+                    if (yes)
+                    {
+
+
+
+                    }
+
+                });
+                // ClientScript.RegisterStartupScript(GetType(), "message", "<script>alert('新增資料OK！');</script>");
+                Response.Redirect("DepartLst.aspx");
+            }
+            catch (Exception ex)
+            {
+                // MessageBox.Show(sqlString + ex.Message);
+                Response.Write("新增資料錯誤!"+sqlString + ex.Message);
+            }
+        }
+
+        protected void btnCancel_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("DepartLst.aspx");
+        }
+    }
+}
